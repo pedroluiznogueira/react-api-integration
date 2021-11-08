bfb3b7eb2d01418273e38ff91eb9c35dde1d3023
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-11-08 20:17:32 -0300

    Next, import the service, then call the service inside your useEffect Hook. Update the list with setList if the component is mounted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getList } from './services/list';
 
 function App() {
   const [list, setList] = useState([]);
+
+  useEffect(() => {
+   let mounted = true;
+   getList()
+     .then(items => {
+       if(mounted) {
+         setList(items)
+         console.log(list)
+       }
+     })
+   return () => mounted = false;
+ }, [])
+
   return(
     <>
     </>
